3201ef8121de65c07911b0686db0b29a9924dc58
Alwan Fauzi <alwan.fauzi.ft21@mail.umy.ac.id>
2023-12-27 18:05:41 +0700

    add deleteKontak di ondeleteclick

--- app/src/main/java/com/example/consumerestapi/ui/theme/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/consumerestapi/ui/theme/home/screen/HomeScreen.kt
@@ -64,7 +64,7 @@ fun HomeScreen(
 
             onDetailClick = onDetailClick,
             onDeleteClick = {
-
+                viewModel.deleteKontak(it.id)
                 viewModel.getKontak()
             }
         )
